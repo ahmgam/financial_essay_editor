41f2de35b4b08120ae2241c9f159f1afc2a0fd44
ahmgam <50333822+ahmgam@users.noreply.github.com>
2020-08-17 00:58:48 +0200

    nocption

--- finantial_editor/editor/views.py
+++ finantial_editor/editor/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 import requests
 # Create your views here.
 def index (request):
-    return render(request, 'polls/index.html')
+    return render(request, 'edit/edit.html')
+
 def preview_chart(request):
     
     mode,start_date,end_date,ticker,stock=""
@@ -19,6 +20,9 @@ def preview_chart(request):
     
 def getData(mode,start_date,end_date,stock,ticker):
 
+    if ticker=="" and stock=="":
+        res = "Error, please enter valid ticker code"
+
     if mode != "eod" & mode != "intraday":
         res = "error , please enter a valid ticker name"
         #throw mode error
@@ -27,37 +31,61 @@ def getData(mode,start_date,end_date,stock,ticker):
         if ticker!="" and stock=="":
             if start_date =="" and end_date =="": 
                 # get the latest data 
-                r = requests.get('https://api.marketstack.com/v1/'+mode+'/latest?access_key=27f1205b8dcf44c54526e45b80a7b8f0&symbols='+ticker)
-            if start_date=="" and end_date!="":
+                r = 'http://api.marketstack.com/v1/intraday/latest?access_key=27f1205b8dcf44c54526e45b80a7b8f0&symbols='+ticker
+            if start_date=="" and end_date!=="":
                 # get for end date only
-                r = requests.get('https://api.marketstack.com/v1/'+mode+'/'+end_date+'?access_key=27f1205b8dcf44c54526e45b80a7b8f0&symbols='+ticker)
+                r = 'http://api.marketstack.com/v1/intraday/'+end_date+'?access_key=27f1205b8dcf44c54526e45b80a7b8f0&symbols='+ticker
 
-            if end_date=="" and start_date!="":
+            if end_date=="" and start_date!=="":
                 # get for start date only
-                r = requests.get('https://api.marketstack.com/v1/'+mode+'/'+start_date+'?access_key=27f1205b8dcf44c54526e45b80a7b8f0&symbols='+ticker)
+                r = 'http://api.marketstack.com/v1/intraday/'+start_date+'?access_key=27f1205b8dcf44c54526e45b80a7b8f0&symbols='+ticker
 
-            if end_date!="" and start_date!="":
+            if end_date!="" and start_date!=="":
                 # get for the entire period
-                r = requests.get('https://api.marketstack.com/v1/'+mode+'/?access_key=27f1205b8dcf44c54526e45b80a7b8f0&symbols='+ticker+'&date_from='+start_date+'&date_to='+ end_date)
+                r = 'http://api.marketstack.com/v1/eod/?access_key=27f1205b8dcf44c54526e45b80a7b8f0&symbols='+ticker+'&date_from='+start_date+'&date_to='+ end_date
 
             
         if ticker=="" and stock!="":
             if start_date =="" and end_date =="": 
                 # get the latest data 
-                r = requests.get('https://api.marketstack.com/v1/exchanges/'+stock+'/'+mode+'/latest?access_key=27f1205b8dcf44c54526e45b80a7b8f0')
+                r = 'http://api.marketstack.com/v1/exchanges/'+stock+'/'+mode+'/latest?access_key=27f1205b8dcf44c54526e45b80a7b8f0'
             if start_date=="" and end_date!="":
                 # get for end date only
-                r = requests.get('https://api.marketstack.com/v1/exchanges/'+stock+'/'+mode+'/'+ end_date+'?access_key=27f1205b8dcf44c54526e45b80a7b8f0')
+                r = 'http://api.marketstack.com/v1/exchanges/'+stock+'/'+mode+'/'+ end_date+'?access_key=27f1205b8dcf44c54526e45b80a7b8f0'
             if end_date=="" and start_date!="":
                 # get for start date only
-                r = requests.get('https://api.marketstack.com/v1/exchanges/'+stock+'/'+mode+'/'+ start_date+'?access_key=27f1205b8dcf44c54526e45b80a7b8f0')
+                r = 'http://api.marketstack.com/v1/exchanges/'+stock+'/'+mode+'/'+ start_date+'?access_key=27f1205b8dcf44c54526e45b80a7b8f0'
             if end_date!="" and start_date!="":
                 # get for the entire period
-                r = requests.get('https://api.marketstack.com/v1/exchanges/'+stock+'/'+mode+'/'+ end_date+'?access_key=27f1205b8dcf44c54526e45b80a7b8f0')
+                r = 'http://api.marketstack.com/v1/exchanges/'+stock+'/'+mode+'/'+ end_date+'?access_key=27f1205b8dcf44c54526e45b80a7b8f0'
         
-        if ticker=="" and stock=="":
-            #throw error
     return HttpResponse(res)
+
+def processData (route,mode):
+    data = []
+    offset = 0
+    while (True):
+        rawData = requests.get(route + '&offset=' +str(offset)).json()
+        if "error" in rawData
+            return "error: " + rawData["error"]["message"]
+        totalData = int(rawData['pagination']['total'])
+        batchSize = int(rawData['pagination']['count'])
+        limit = int(rawData['pagination']['limit'])
+        requestOffset = int(rawData['pagination']['offset'])
+        for d in rawData['data']:
+            data.append(d)
+        if (totalData - (batchSize + requestOffset)) <= 0:
+            print(data)
+            break
+        offset = int(rawData['pagination']['offset']) + batchSize
+    
+    
+
+
+
+
+
+    
 
 
 
